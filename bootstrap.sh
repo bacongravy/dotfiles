427481aee4592aa17e1f102d289fb8a4bcb71b72
David Kramer <bacongravy@icloud.com>
2016-02-21 00:04:20 -0800

    bootstrap.sh only works when run from ~

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,11 +6,11 @@ fi
 
 for file in {bash_profile,bashrc,tmux.conf,vimrc}; do
   source=.dotfiles/$file;
-  destination=.$file;
+  destination=~/.$file;
   if [ -e "$destination" ] && [ -z "$force" ]; then
     echo "Skipping ~/.$file, file already exists. Use --force to override." 1>&2;
   else
-    (cd ~; ln -f -s "$source" "$destination");
+    ln -f -s "$source" "$destination";
   fi;
 done;
 
